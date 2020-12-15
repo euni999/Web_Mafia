83178c43ff3cdd63b2d2b9475708b9ab73032284
Marker <56999067+Marker0724@users.noreply.github.com>
2020-12-15 19:42:08 +0900

    회원가입 페이지

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 let signupRouter = require('./routes/signup');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const bodyParser = require("body-parser");
 
 var app = express();
 
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '')));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
